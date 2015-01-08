fa023a69193c76eedb15b203657650d204c51245
Eric Stutzenberger <eric.stutzenberger@rigado.com>
2015-01-08 10:35:13 -0800

    add rigablue.h

---
+++ iOS/Rigablue/Rigablue.h
@@ -0,0 +1,17 @@
+//
+//  Rigablue.h
+//  Rigablue
+//
+//  Created by Eric P. Stutzenberger on 1/8/15.
+//  Copyright (c) 2015 Rigado, LLC. All rights reserved.
+//
+
+#ifndef _RIGABLUE_H_
+#define _RIGABLUE_H_
+#endif
+
+#import "RigAvailableDeviceData.h"
+#import "RigDeviceRequest.h"
+#import "RigLeBaseDevice.h"
+#import "RigLeDiscoveryManager.h"
+#import "RigLeConnectionManager.h"
