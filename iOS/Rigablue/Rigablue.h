34d5ba6a2b331862a7cf9d057cdbf9c6ae8b50c4
Eric Stutzenberger <eric.stutzenberger@rigado.com>
2015-06-11 11:37:26 -0700

    add Rigablue.h

--- iOS/Rigablue/Rigablue.h
+++ iOS/Rigablue/Rigablue.h
@@ -1,19 +1,18 @@
 //
-//  @file Rigablue.h
-//  @library Rigablue
+//  Rigablue.h
+//  LullyDemo
 //
-//  @copyright (c) 2014-2015 Rigado, LLC. All rights reserved.
-//  Source code licensed under BMD-200 Software License Agreement.
-//  You should have received a copy with purchase of BMD-200 product.
-//  If not, contact info@rigado.com for for a copy.
+//  Created by Eric P. Stutzenberger on 6/3/15.
+//  Copyright (c) 2015 Rigado,LLC. All rights reserved.
+//
 
-#ifndef _RIGABLUE_H_
-#define _RIGABLUE_H_
+#ifndef __RIGABLUE_H__
+#define __RIGABLUE_H__
 
-#import "RigAvailableDeviceData.h"
-#import "RigDeviceRequest.h"
-#import "RigLeBaseDevice.h"
 #import "RigLeDiscoveryManager.h"
 #import "RigLeConnectionManager.h"
-
+#import "RigLeBaseDevice.h"
+#import "RigDeviceRequest.h"
+#import "RigAvailableDeviceData.h"
+#import "RigFirmwareUpdateManager.h"
 #endif
