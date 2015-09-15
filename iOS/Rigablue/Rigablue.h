dc3c20d23ce77d359e2ebf263026b8e272ec4989
Eric Stutzenberger <eric.stutzenberger@rigado.com>
2015-09-15 16:43:01 -0700

    Revert "Revert "Revert "add Rigablue.h"""
    This reverts commit 6148732838949b31bda22a9b6fbd68e0214f5f43.

--- iOS/Rigablue/Rigablue.h
+++ iOS/Rigablue/Rigablue.h
@@ -1,18 +1,19 @@
 //
-//  Rigablue.h
-//  LullyDemo
+//  @file Rigablue.h
+//  @library Rigablue
 //
-//  Created by Eric P. Stutzenberger on 6/3/15.
-//  Copyright (c) 2015 Rigado,LLC. All rights reserved.
-//
+//  @copyright (c) 2014-2015 Rigado, LLC. All rights reserved.
+//  Source code licensed under BMD-200 Software License Agreement.
+//  You should have received a copy with purchase of BMD-200 product.
+//  If not, contact info@rigado.com for for a copy.
 
-#ifndef __RIGABLUE_H__
-#define __RIGABLUE_H__
+#ifndef _RIGABLUE_H_
+#define _RIGABLUE_H_
 
+#import "RigAvailableDeviceData.h"
+#import "RigDeviceRequest.h"
+#import "RigLeBaseDevice.h"
 #import "RigLeDiscoveryManager.h"
 #import "RigLeConnectionManager.h"
-#import "RigLeBaseDevice.h"
-#import "RigDeviceRequest.h"
-#import "RigAvailableDeviceData.h"
-#import "RigFirmwareUpdateManager.h"
+
 #endif
