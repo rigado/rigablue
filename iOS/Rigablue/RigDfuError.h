963a286a04421fb856eae30de28d547dcb2d0320
momoko <momoko.saunders@rigado.com>
2017-02-10 15:03:11 -0800

    Add Comments for Errors

--- iOS/Rigablue/RigDfuError.h
+++ iOS/Rigablue/RigDfuError.h
@@ -15,17 +15,40 @@
 //Error code for potential failure points within the firmware update manager
 typedef enum DfuErrorEnum
 {
+    // No Error
     DfuError_None = 0,
+    
+    // Could not find RigDfu device. Bad Peripheral.
     DfuError_BadPeripheral = -1,
+    
+    // Could not initialize firmware update service. Missing control point characteristic.
     DfuError_ControlPointCharacteristicMissing = -2,
+    
+    // Could not find RigDfu device. Bad Device.
     DfuError_BadDevice = -3,
+    
+    // Could not find RigDfu device. Peripheral not set.
     DfuError_PeripheralNotSet = -4,
+    
+    // Invalid parameter in updateFirmware command.
     DfuError_InvalidParameter = -5,
+    
+    // Failed to validate firmware image.
     DfuError_ImageValidationFailure = -6,
+    
+    // Failed to activate firmware image.
     DfuError_ImageActivationFailure = -7,
+    
+    // CRC for the current firmware image does not match required CRC. Is it the correct patch version?
     DfuError_PatchCurrentImageCrcFailure = -8,
+    
+    // CRC for the updated firmware image does not match the required CRC. Is it the correct patch version?
     DfuError_PostPatchImageCrcFailure = -9,
+    
+    // Could not connect to RigDfu device.
     DfuError_CouldNotConnect = -10,
+    
+    // An unknown error occured.
     DfuError_Unknown = -11,
     
 } RigDfuError_t;
